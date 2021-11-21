9869cc9f122d6842cd50d75b34b97d1f497073d6
Ilia Zolas <ilia@thedmcsa.co.za>
2021-11-21 17:10:09 +0100

    update nav

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Button from './components/UI/buttons/buttons';
 import Nav from './components/UI/nav/nav';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import About from './components/Pages/about/about';
 import Contact from './components/Pages/contact/contact';
 import Home from './components/Pages/home/home';
--- src/components/UI/nav/nav.js
+++ src/components/UI/nav/nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './nav.css';
 import './assets/images/logo.png'; 
 import img from './assets/images/logo.png';
+import {Link} from 'react-router-dom'
 
 
 const Nav = (props) => {
@@ -12,15 +13,15 @@ const Nav = (props) => {
         </div>
         <div className="nav-links-right">
             <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/about">About</a>
-                </li>
-                <li>
-                    <a href="/contact">Contact</a>
-                </li>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/about">
+                    <li>About</li>
+                </Link>
+                <Link to="/contact">
+                    <li>Contact</li>
+                </Link>
             </ul>
         </div>
     </div>
