56ef90f9bc80a969fcd5d061dd5cb56674ad9f46
Ilia Zolas <ilia@thedmcsa.co.za>
2021-11-28 18:13:28 +0100

    connection successful

--- server/index.js
+++ server/index.js
@@ -1,23 +1,43 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const mysql = require("mysql");
+const mysql = require('mysql');
+// const bodyParser = require('body-parser');
 
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database:'band_reviews',
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "band_reviews"
+});
+
+db.connect((err) => {
+    if(err){
+        console.log('Error connecting to Db');
+    } else { 
+        console.log('Connection Established');
+    }   
 });
 
 app.get("/", (req,res) => {
-    const sqlInsert = "INSERT INTO band_reviews (bandName, bandReview) VALUES ('Metallica', 'Good Band');";
+    const sqlInsert = "INSERT INTO band_reviews (bandName, bandReview) VALUES ('Metallica','Really cool');";
     db.query(sqlInsert, (err, result) => {
-    res.send("hello Goku");
+        if (!err) {
+            console.log('Successfully added information.');
+            } else {
+            console.log('Was not able to add information to database.');
+        }
+        res.send("Testing DB Connection");
     });
-
 });
 
+db.end(function (err){
+    //
+});
 
 app.listen(3001, () => {
-    console.log('running on port 3001');
-});
+        console.log('running on port 3001');
+    });
+
+
+    
